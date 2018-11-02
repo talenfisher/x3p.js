bbf9df455acbc0ec4255e27bdffcd4bf1f61e642
Talen Fisher <tqfisher@iastate.edu>
2018-11-02 12:19:57 -0500

    create empty x3p when file param = null

--- src/x3p.js
+++ src/x3p.js
@@ -1,5 +1,31 @@
+import JSZip from "jszip";
+
 export default class X3P {
-    constructor() {
-        
+    /**
+     * 
+     * @param {File|null} file optional file to load
+     * @param {*} name 
+     */
+    constructor(file = null, name = null) {
+        this._file = new JSZip();
+        this.name = name;
+
+        if(file !== null) {
+            this._file.loadAsync(file);
+            return;
+        }
+
+        // TODO: add defaults for each file
+        this._file.file("main.xml", "");
+        this._file.file("bindata/data.bin", "");
+        this._file.file("md5checksum.hex", "");
+    }
+
+    /**
+     * Getter for file
+     * @return {JSZip} the X3P file as a JSZip Object
+     */
+    get container() {
+        return this._file;
     }
 }
--- tests/x3p.js
+++ tests/x3p.js
@@ -1,5 +1,13 @@
 const assert = require("assert");
+const X3P = require("../bin/x3p");
 
 describe("X3P", () => {
-    // placeholder for future tests
+    describe("constructor", () => {
+        it("should create new X3P file when file parameter is not specified", () => {
+            let x3p = new X3P();
+            assert.equal("main.xml" in x3p.container.files, true);
+            assert.equal("bindata/data.bin" in x3p.container.files, true);
+            assert.equal("md5checksum.hex" in x3p.container.files, true);
+        });
+    });
 });
