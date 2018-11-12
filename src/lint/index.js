5112d498bc7df54989a0b26a1dcb501d7f9f5fbd
Talen Fisher <tqfisher@iastate.edu>
2018-11-12 09:27:29 -0600

    remove linting for now

--- src/lint/index.js
+++
@@ -1,9 +0,0 @@
-import schema from "./iso5436_2.xml";
-import { validateXML } from "@talenfisher/xmllint";
-
-export default function lint(xml) {
-    return validateXML({
-        xml, 
-        schema 
-    });
-}
