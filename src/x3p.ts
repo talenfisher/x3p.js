e971415738b28486ba98145090ee3c65f8225d3c
Talen Fisher <tqfisher@iastate.edu>
2019-01-24 15:03:46 -0600

    mock URL.createObjectURL as a noop

--- src/x3p.ts
+++ src/x3p.ts
@@ -21,6 +21,7 @@ export default class X3P {
     public pointBuffer?: ArrayBuffer;
     public mask: Mask;
     public matrix?: ndarray;
+    public saveMask: boolean = true; // use for testing/debugging
 
     private loader: X3PLoader;
     private options: X3POptions;
@@ -51,8 +52,8 @@ export default class X3P {
         this.loader.write("main.xml", this.manifest.toString());
         this.loader.write("md5checksum.hex", `${this.manifest.checksum} *main.xml`);
 
-        let canvas = this.mask.canvas;
-        if(canvas) { // save mask
+        if(this.saveMask && this.mask.canvas) { // save mask
+            let canvas = this.mask.canvas;
             let blob = await canvas.toBlob();
             this.loader.write("bindata/mask.png", blob);
         }
