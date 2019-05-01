0edca53f8a9b396a160533a8f8fd465d1362644c
Talen Fisher <tqfisher@iastate.edu>
2019-05-01 00:02:43 -0500

    cleanup data types

--- src/data-types.ts
+++ src/data-types.ts
@@ -8,33 +8,22 @@ export interface DataType {
     bytes: number;
 }
 
-export function bufferToTypedArray(buffer: ArrayBuffer, type: DataTypeName): TypedArray {
-    let result;
-    
-    switch(type) {
-        default:
-        case "Float64": result = new Float64Array(buffer); break;
-        case "Float32": result = new Float32Array(buffer); break;
-        case "Int32": result = new Int32Array(buffer); break;
-        case "Int16": result = new Int16Array(buffer); break;
-    }
-
-    return result;
-}
-
 export default {
     D: { 
         name: "Float64", 
         bytes: 8,
     } as DataType,
+
     F: { 
         name: "Float32", 
         bytes: 4,
     } as DataType,
+
     L: { 
         name: "Int32", 
         bytes: 4,
     } as DataType,
+    
     I: { 
         name: "Int16", 
         bytes: 2,
